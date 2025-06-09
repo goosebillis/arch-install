92e4a378b421c925f2202a62de918534aa7aacad
Goose <202384782+goosebillis@users.noreply.github.com>
2025-06-09 18:39:19 +0100

    Update hyprland.sh

--- hyprland.sh
+++ hyprland.sh
@@ -12,5 +12,16 @@ sudo ./cachyos-repo.sh
 
 sudo pacman -Syu 
 
+sudo pacman -S cachyos-rate-mirrors ungoogled-chromium-bin
+sudo cachyos-rate-mirrors
+sudo vim /etc/pacman.d/mirrorlist
+sudo vim /etc/pacman.d/cachyos-mirrorlist
+sudo vim /etc/pacman.d/cachyos-v3-mirrorlist
+sudo vim /etc/pacman.d/cachyos-v4-mirrorlist
+
 cd ~/.setup
-sh <(curl -L https://raw.githubusercontent.com/JaKooLit/Arch-Hyprland/main/auto-install.sh)
+git clone --depth=1 https://github.com/JaKooLit/Arch-Hyprland.git ~/.setup/Arch-Hyprland
+cd ~/.setup/Arch-Hyprland
+cp ~/arch-install/nvidia.sh ~/.setup/Arch-Hyprland/install-scripts/
+chmod +x install.sh
+./install.sh
