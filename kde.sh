4b5d8b12dc7e7010b9e029c97234df9543bd6594
Goose <202384782+goosebillis@users.noreply.github.com>
2025-07-26 07:51:43 +0100

    Update kde.sh

--- kde.sh
+++ kde.sh
@@ -24,23 +24,7 @@ git clone https://aur.archlinux.org/paru-bin
 cd paru-bin
 makepkg -si
 
-paru -S nvidia-open-dkms nvidia-settings nvidia-utils libva libva-nvidia-driver
-
-# add NVIDIA to mkinitcpio.conf
-sudo sed -Ei 's/^(MODULES=\([^\)]*)\)/\1 nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
-
-# Additional Nvidia steps
-sudo echo -e "options nvidia_drm modeset=1 fbdev=1" | sudo tee -a /etc/modprobe.d/nvidia.conf
-
-# Blacklist nouveau
-echo "blacklist nouveau" | sudo tee -a /etc/modprobe.d/nouveau.conf
-echo "install nouveau /bin/true" | sudo tee "/etc/modprobe.d/blacklist.conf"
-
-# Add GRUB Entries
-sudo sed -i -e 's/\(GRUB_CMDLINE_LINUX_DEFAULT=".*\)"/\1 nvidia_drm.fbdev=1"/' /etc/default/grub
-
-sudo mkinitcpio -P
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+paru -S mesa lib32-mesa vulkan-intel lib32-vulkan-intel
 
 paru -S findutils archlinux-keyring base-devel jq qt6ct yad btop fastfetch pacman-contrib yt-dlp imagemagick qt6-sv qt5ct wayland qt5-wayland qt6-wayland xorg-xwayland xwaylandvideobridge plasma-meta gtk-engine-murrine adobe-source-code-pro-fonts noto-fonts-emoji otf-font-awesome ttf-droid ttf-fira-code ttf-fantasque-nerd ttf-jetbrains-mono ttf-jetbrains-mono-nerd ttf-victor-mono noto-fonts bluez bluez-utils kate ark dolphin dolphin-plugins okular pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse sof-firmware ffmpegthumbs kdeconnect partitionmanager kdialog kitty flatpak-kcm p7zip unrar unzip sddm firefox
 
