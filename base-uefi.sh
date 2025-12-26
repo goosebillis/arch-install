249e38b17a9bc88319c31a07cde21ed5dc78f82a
Goose <202384782+goosebillis@users.noreply.github.com>
2025-12-26 18:55:24 +0000

    Update base-uefi.sh

--- base-uefi.sh
+++ base-uefi.sh
@@ -14,9 +14,9 @@ umount /mnt
 mount -o rw,noatime,compress=zstd:1,ssd,discard=async,space_cache=v2,autodefrag,subvol=@ /dev/nvme0n1p3 /mnt
 mkdir -p /mnt/{boot,home}
 mount -o rw,noatime,compress=zstd:1,ssd,discard=async,space_cache=v2,autodefrag,subvol=@home /dev/nvme0n1p3 /mnt/home
-mount -o rw,noatime /dev/sdb2 /mnt/boot
+mount -o rw,noatime /dev/nvme0n1p2 /mnt/boot
 mkdir -p /mnt/boot/efi
-mount /dev/sdb1 /mnt/boot/efi
+mount /dev/nvme0n1p1 /mnt/boot/efi
 
 pacstrap -K /mnt base base-devel linux linux-firmware linux-headers amd-ucode intel-ucode git btrfs-progs grub efibootmgr grub-btrfs network-manager-applet dialog wpa_supplicant mtools dosfstools ntfs-3g inotify-tools timeshift vim networkmanager pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber reflector openssh man sudo xdg-user-dirs xdg-utils bluez bluez-utils cups bash-completion openssh rsync acpi acpi_call firewalld flatpak sof-firmware nss-mdns acpid os-prober terminus-font
 
