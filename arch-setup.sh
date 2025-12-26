eae7e1c7a93a2a7788585be7202c9c1d2caae228
Goose <202384782+goosebillis@users.noreply.github.com>
2025-12-26 18:54:37 +0000

    Update arch-setup.sh

--- arch-setup.sh
+++ arch-setup.sh
@@ -5,17 +5,19 @@ vim /etc/locale.gen
 locale-gen
 echo "LANG=en_GB.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=uk" >> /etc/vconsole.conf
-echo "arch" >> /etc/hostname
+echo "theb3ast" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
-echo root:Mar5ha11!. | chpasswd
+echo "127.0.1.1 theb3ast.localdomain theb3ast" >> /etc/hosts
+echo root:[PASSWORD] | chpasswd
 
 useradd -mG wheel david
-echo david:9791 | chpasswd
+echo david:[PASSWORD] | chpasswd
 EDITOR=vim visudo
 
 echo "david ALL=(ALL) ALL" >> /etc/sudoers.d/david
+
+mkinitcpio -P linux
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
